a78332d9d47f7e1bd4a2b2d87cc6a073575d934b
Haimaimon <105820824+Haimaimon@users.noreply.github.com>
2022-12-27 14:18:44 +0200

    login page and models,front and beck

--- Job_Tech/views.py
+++ Job_Tech/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from tempfile import tempdir
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'username OR password is incorrect')
+    context = {}
+    return render(request, 'login.html', context)
