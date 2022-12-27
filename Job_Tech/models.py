f7272bcef544759dc6d2bd958176a1f2fdfba23b
Linoy-tur <117343315+Linoy-tur@users.noreply.github.com>
2022-12-27 17:40:40 +0200

    models sjob student

--- Job_Tech/models.py
+++ Job_Tech/models.py
@@ -8,3 +8,13 @@ class Job(models.Model):
 
     def __str__(self):
         return f'{self.title}, {self.desc}, {self.company}'
+
+
+class StudentJobs(models.Model):
+    title = models.CharField(max_length=80)
+    desc = models.TextField()
+    company = models.CharField(max_length=40)
+    location = models.CharField(max_length=30)
+
+    def _str_(self):
+        return f'{self.title}, {self.desc}, {self.company},{self.location}'
