fccfbe70527bd0d16452703199a7bdee8b1f4838
Lidorkatzav <Lidorka@ac.sce.ac.il>
2023-01-13 00:04:40 +0200

    images and some tests

--- Job_Tech/tests.py
+++ Job_Tech/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+    #check urls of a local server pages
+    def test_homepage(self):
+        response=self.cilent.get('/')
+
+
+    #check Register page
+    def test_Register_Page(self):
+        response = self.client.get('Register/')
+        self.assertEqual(response.status_code, 404)
+
+    #check General Register page
+    def test_Success_Page(self):
+        response = self.client.get('Success/')
+        self.assertEqual(response.status_code, 404)
+
+    #check Manager Register page
+    def test_Reg_Page(self):
+        response = self.client.get('Reg')
+        self.assertEqual(response.status_code, 404)
